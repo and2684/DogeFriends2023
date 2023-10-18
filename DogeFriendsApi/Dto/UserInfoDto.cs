128a4f0dcd858682e37054120bf54bdc2f4f01db
and2684 <aaaa6226@gmail.com>
2023-10-18 23:29:48 +0500

    UserInfoDto => Id added

--- DogeFriendsApi/Dto/UserInfoDto.cs
+++ DogeFriendsApi/Dto/UserInfoDto.cs
@@ -4,6 +4,7 @@ namespace DogeFriendsApi.Dto
 {
     public class UserInfoDto
     {
+        public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string Showname { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
