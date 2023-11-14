cc3969427d7d52226ca285a230c949ae42e0f131
and2684 <aaaa6226@gmail.com>
2023-11-15 01:41:23 +0500

    main page done

--- AngularDogeFriendsClient/src/app/app.module.ts
+++ AngularDogeFriendsClient/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { LeftPanelComponent } from './components/UI/left-panel/left-panel.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { BaseComponent } from './components/base/base.component';
+import { BreedsComponent } from './components/breeds/breeds.component';
+import { FriendsComponent } from './components/friends/friends.component';
+import { MatSidenavModule} from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    LeftPanelComponent
+    LeftPanelComponent,
+    BaseComponent,
+    BreedsComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +31,10 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
